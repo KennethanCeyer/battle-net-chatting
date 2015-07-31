d59f9ef822453b836a103a329747a2eab71003d9
smhan <smhan@simsimi.com>
2015-07-31 15:47:01 +0900

    First commit.

---
+++ M16Chat-Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace M16Chat_Windows
+{
+
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow
+    {
+        private async void ShowLoginDialog(object sender, RoutedEventArgs e)
+        {
+            LoginDialogData result = await this.ShowLoginAsync("로그인을 해주세요", "M16 계정의 아이디와 비밀번호를 입력해주세요", new LoginDialogSettings { ColorScheme = this.MetroDialogOptions.ColorScheme, AnimateShow = true });
+            if (result == null || (result.Username == null || result.Password == null))
+            {
+                this.ShowLoginDialog(sender, e);
+            }
+            else
+            {
+                MessageDialogResult messageResult = await this.ShowMessageAsync("Authentication Information", String.Format("Username: {0}\nPassword: {1}", result.Username, result.Password));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Initializing(object sender, RoutedEventArgs e)
+        {
+            this.ShowLoginDialog(sender, e);
+        }
+    }
+}
